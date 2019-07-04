04ca5e801937f5b324887ff8bdbc75c91be77e18
nicknoi Tencent <chaiwat@tencent.co.th>
2019-07-04 23:41:38 +0700

    [master] adjust more rules in Lint & Prettier

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   exportPathMap: async function(defaultPathMap) {
     return {
       '/': { page: '/' },
-    };
-  }
+    }
+  },
 }
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
 export default function Home() {
+  const xBool = 2 > 2
+  const hello =
+    xBool === true ? 'asd;fjasdklfjalsdjfldsajflasd' : 'asdfdasfasdfasdfsdfsa'
   return <div>Home</div>
 }
